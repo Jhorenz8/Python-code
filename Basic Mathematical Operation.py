6f575e597cc6c390a279ffaf15ed190e9aafe41c
Jhorenz8 <24-42303@g.batstate-u.edu.ph>
2025-11-20 07:44:42 +0800

    Basic Mathematical Operation.py

---
+++ Basic Mathematical Operation.py
@@ -0,0 +1,21 @@
+
+a = 20
+b = 10
+
+add_result = a + b
+print("Addition:", add_result)
+
+sub_result = a - b
+print("Subtraction:", sub_result)
+
+mul_result = a * b
+print("Multiplication:", mul_result)
+
+div_result = a / b
+print("Division:", div_result)
+
+mod_result = a % b
+print("Modulo:", mod_result)
+
+exp_result = a ** b
+print("Exponent:", exp_result)
