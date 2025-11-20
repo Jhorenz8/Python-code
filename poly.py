fadd7c22f1dfbf5e65d73a3c539ec0b2389c3de2
Jhorenz8 <24-42303@g.batstate-u.edu.ph>
2025-11-20 08:35:49 +0800

    poly

---
+++ poly.py
@@ -0,0 +1,16 @@
+class InkPrinter:
+    def print_document(self):
+
+        print("Sprays liquid ink droplets through tiny nozzles.")
+
+
+class LaserPrinter:
+    def print_document(self):
+        print(
+            "Uses a laser to charge a drum, which attracts powdered toner to the paper, and then uses heat to fuse it."
+        )
+
+
+printers = [InkPrinter(), LaserPrinter()]
+for printer in printers:
+    printer.print_document()
